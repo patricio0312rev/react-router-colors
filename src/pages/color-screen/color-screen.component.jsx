6a4dfac3f17033b9b26c32b8a4a72fcf8e8123b1
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-16 17:30:04 -0500

    Proyecto finalizado

--- src/pages/color-screen/color-screen.component.jsx
+++ src/pages/color-screen/color-screen.component.jsx
@@ -5,8 +5,16 @@ import './color-screen.styles.css';
 export default function ColorScreenPage() {
     const { color } = useParams();
     return (
-        <div>
-            {color}
+        <div style={{ backgroundColor: `${color}`, height: '100vh', width: '100vw' }}>
+            as
         </div>
     )
+
+    
+    // Other way to do it using Class Components: 
+    /*
+        ColorScreenPage(props)
+        <div style={{ backgroundColor: props.match.params.color }}></div>  
+    */
+
 }
